c4cf533e7bceb599cd1fe1a20be0d968a1c57c1a
Learn&Grow <37178423+SanjivYadav@users.noreply.github.com>
2022-09-15 18:57:03 +0530

    Time: 620 ms (6.59%), Space: 134.6 MB (56.14%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
+       int n = changed.length, i = 0;
+        if (n % 2 == 1) return new int[0];
+        int[] res = new int[n / 2];
+        Map<Integer, Integer> count = new TreeMap<>();
+        for (int a : changed)
+            count.put(a, count.getOrDefault(a, 0) + 1);
+        for (int x : count.keySet()) {
+            if (count.get(x) > count.getOrDefault(x + x, 0))
+                return new int[0];
+            for (int j = 0; j < count.get(x); ++j) {
+                res[i++] = x;
+                count.put(x + x, count.get(x + x) - 1);
+            }
+        }
+        return res;
+    }
+}
