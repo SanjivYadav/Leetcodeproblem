2ef6d9e7391702518cbf8de7530aa45e0820c883
Learn&Grow <37178423+SanjivYadav@users.noreply.github.com>
2022-10-06 20:08:25 +0530

    Time: 1 ms (88.29%), Space: 45.6 MB (33.11%) - LeetHub

---
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {    
+public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            TreeNode newroot = new TreeNode(val);
+            newroot.left = root;
+            return newroot;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        for (int i = 0; i < depth-2; i++) {
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                TreeNode t = queue.poll();
+                if (t.left != null) queue.add(t.left);
+                if (t.right != null) queue.add(t.right);
+            }
+        }
+        while (!queue.isEmpty()) {
+            TreeNode t = queue.poll();
+            TreeNode tmp = t.left;
+            t.left = new TreeNode(val);
+            t.left.left = tmp;
+            tmp = t.right;
+            t.right = new TreeNode(val);
+            t.right.right = tmp;
+        }
+        return root;
+    }
+}
